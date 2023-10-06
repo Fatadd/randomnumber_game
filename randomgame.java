c039521f71e9a5e8af50d97f3318a85b4a77d2d8
fatima addow <fatima.addow@hotmail.com>
2023-10-06 17:32:52 +0200

    Add java.util.random add class and add void main

--- randomgame.java
+++ randomgame.java
@@ -1,1 +1,9 @@
-// hello world
+import java.util.Random;
+import java.util.Scanner;
+
+public class GissaEttTal {
+
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random ();
+    }
